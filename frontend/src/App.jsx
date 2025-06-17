935314f2629359e9db15ce506268b1be55fd46af
sufiyansayyed19 <sufiyan.sayed92@example.com>
2025-06-17 15:57:08 +0530

    fronend struture initialized

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/layout/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-slate-900 min-h-screen">
+      <Navbar />
+      {/* The rest of our page content will go here */}
+      <div className="flex justify-center items-center h-[calc(100vh-80px)]">
+        <h1 className="text-3xl font-bold text-white">
+          Page Content Area
+        </h1>
       </div>
-      <h1 className='text-red-400'>Vite + React</h1>
-      <h1 class="text-blue-500">This is a Blue Heading</h1>
-<h2 class="text-green-600">This is a Green Subheading</h2>
-<h3 class="text-purple-700">This is a Purple Title</h3>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
