fc7afd07956651d2d979be77db719131aa0c69f9
sufiyansayyed19 <sufiyan.sayed92@gmail.com>
2025-06-24 21:29:30 +0530

    App.jsx updated

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -1,19 +1,13 @@
 // src/App.jsx
-import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import ProductManagementPage from './pages/ProductManagementPage';
+import BlogManagementPage from './pages/BlogManagementPage'; // <-- Import new page
 
-// Define routes
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LoginPage />,
-  },
-  {
-    path: "/products",
-    element: <ProductManagementPage />,
-  },
-  // We will add the /blogs route here later
+  { path: "/", element: <LoginPage /> },
+  { path: "/products", element: <ProductManagementPage /> },
+  { path: "/blogs", element: <BlogManagementPage /> }, // <-- Add new route
 ]);
 
 function App() {
