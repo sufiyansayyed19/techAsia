1f2faeaec46d1432064d35bc32612c752fba8607
sufiyansayyed19 <sufiyan.sayed92@gmail.com>
2025-06-24 20:06:42 +0530

    admin panel initialized

---
+++ admin-panel/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <h1 className="text-2xl font-bold text-center p-8">Admin Panel</h1>
+    </div>
+  )
+}
+
+export default App
