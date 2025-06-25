160152f4c14eb28cf3a54998809b9fe0d811b9c9
sufiyansayyed19 <sufiyan.sayed92@gmail.com>
2025-06-26 01:10:21 +0530

    in productRoutes.jsx updated and both update and delete are tested using postman

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 // routes/productRoutes.js
 import express from 'express';
-import { getProducts, createProduct } from '../controllers/productController.js';
+import { getProducts, createProduct, updateProduct, deleteProduct } from '../controllers/productController.js';
 
 const router = express.Router();
 
@@ -9,5 +9,8 @@ router.route('/').get(getProducts);
 
 // This route will handle POST requests to /api/products
 router.route('/').post(createProduct);
+
+// This handles PUT and DELETE for URLs with an ID, like /api/products/12345
+router.route('/:id').put(updateProduct).delete(deleteProduct);
 
 export default router;
