7737857b2a14e755c0aa260a81568c23dd0fa264
sufiyansayyed19 <sufiyan.sayed92@gmail.com>
2025-06-26 01:25:38 +0530

    temp changes of image in model products.js

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 const productSchema = new mongoose.Schema({
   title: { type: String, required: true },
   slug: { type: String, required: true, unique: true },
-  image: { type: String, required: true }, // This will store the URL of the image
+  image: { type: String }, // Temporarily not required, // This will store the URL of the image
   description: { type: String, required: true },
   additionalFeatures: [String],
   technicalDetails: { type: Map, of: String }
