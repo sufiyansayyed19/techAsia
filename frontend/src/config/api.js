eddf9f01f626e0b7d06d76f3adfcf891cb009040
sufiyansayyed19 <sufiyan.sayed92@gmail.com>
2025-07-04 00:25:49 +0530

    api port updated

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,2 +1,2 @@
 // This file centralizes the API URL for the entire frontend application.
-export const API_BASE_URL = 'http://localhost:5002/api';
+export const API_BASE_URL = 'http://localhost:5001/api';
